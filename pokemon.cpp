c6015fd00002029c8372c02ba180e6257aef4c0f
Yann-Paul <yannpaulm@gmail.com>
2017-08-15 21:50:12 +0200

    Update pokemon.cpp
    Spielerklasse mit Inventar und Pokemonauswahl erstellt.

--- pokemon.cpp
+++ pokemon.cpp
@@ -5,7 +5,13 @@
 using namespace std;
 
 
-int inventory[2] = {5,5}; // Erste Zahl = anzahl Manatränke, Zweite Zahl = anzahl Healthtränke
+string name;  // Spielernamen
+char input; // Eingabe im späteren Input
+int auswahl;
+
+// vector declaration fürs Inventory
+int vv[2] = { 5,5 };
+std::vector<int> v(&vv[0], &vv[0]+2);
 
 class Attack
 {
@@ -48,17 +54,7 @@ public:
 	     cout << "Pokemon wurde erstellt"<<  endl;  // Test
 	}
 
-	void usepotion(bool mahe){ // mahe = 1 -> mana; mahe = 0 -> health
-		if (mahe){
-			mana = 100;
-			inventory[0] --;
-		}
-		else{
-			health = 100;
-			inventory[1] --;
-		}
-	}
-	// zur Einfachheit gibt es erst mal nur eine Sorte an Tränken die health oder mana direkt auf 100 setzt
+
 
 	void levelup(){
 		level ++;
@@ -102,19 +98,46 @@ public:
     	this-> attack1 = attack1;
     	this-> attack2 = attack2;
     };
-
-
-
+    Pokemon(){};
 private:
 };
+
+class Spieler
+{
+public:
+	string name;
+	vector<int> inventory; // = {5,5}; // Erste Zahl = anzahl Manatränke, Zweite Zahl = anzahl Healthtränke;
+	Pokemon pokemon1;
+	Pokemon pokemon2;
+	Pokemon pokemon3;
+	Spieler(string name, Pokemon pokemon1, Pokemon pokemon2, Pokemon pokemon3){
+		this-> name = name;
+		this-> inventory = v;
+		this-> pokemon1 = pokemon1;
+		this-> pokemon2 = pokemon2;
+		this-> pokemon3 = pokemon3;
+	};
+
+	void usepotion(bool mahe, Pokemon pokemon){ // mahe = 1 -> mana; mahe = 0 -> health
+		if (mahe){
+			pokemon.mana = 100;
+			inventory[0] --;
+		}
+		else{
+			pokemon.health = 100;
+			inventory[1] --;
+		}
+	}
+	// zur Einfachheit gibt es erst mal nur eine Sorte an Tränken die health oder mana direkt auf 100 setzt
+};
+
+
 
 
 
 
 int main()
 {
-
-
 	// Attacken (name, typ, strength, precision, manacost)
 	Attack tackle("Tackle", "normal", 40, 100, 20);
 	Attack growl("Growl", "normal", 45, 100, 20);
@@ -127,6 +150,7 @@ int main()
 
 
 	// Erstelle die Pokemons
+	Pokemon none("None", "None", 0, 0, 0, 0, 0.0, 0.0, 0, 0, 0.0, tackle, tackle);
 	Pokemon bisasam("Bisasam", "grass", 001, 1, 100, 100, 0.7, 6.9, 10, 002, 0.0, tackle, growl);
 	Pokemon bisaknosp("Bisaknosp", "grass", 002, 11, 100, 100, 1.0, 13.0, 20, 003, 0.0, solarBeam, vineWhip);
 	Pokemon bisaflor("Bisaflor", "grass", 003, 21, 100, 100, 2.0, 100.0, 30, 003, 0.0, solarBeam, razorLeaf);
@@ -136,16 +160,40 @@ int main()
 
 
 	// fuer spaeter
-	//Pokemon pokedex[3] = { bisasam, bisaknosp, bisaflor };
+	Pokemon pokedex[3] = { bisasam, bisaknosp, bisaflor };
 
 
     //Pokemon auswaehlen
-    // cout << "Willkommen im Pokedex. Bitte waehle eines der folgenden Pokemon aus " << pokedex <<endl;  // Test
-	std::string s = std::to_string(bisasam.attack1.precision);
-     std::cout << s << bisasam.type;  // Test
-    //cin >> input;
+    cout << "Hallo! Um das Spiel beginnen zu können brauchst du einen Namen. Bitte wähle deinen Namen aus. " <<endl; // Test
+    cin >> name;
+    Spieler spieler(name, none, none, none);
+    cout << "Hallo " << name <<"! Herzlich Willkommen in der Pokemonarena. Als nächstes wähle bitte dein Pokemon aus."<<endl;
+    cout << "Dafür drücke eine der folgenden Zahlen."<<endl;
+    cout << "1 = Bisasam        2 = Bisaknosp        3 = Bisaflor"<<endl;
+    cin >> input;
 
+	while (false) {
+		switch (input) {
+		case '1':
+			auswahl = 1;
+			return true;
+		case '2':
+			auswahl = 2;
+			return true;
+		case '3':
+			auswahl = 3;
+			return true;
+		default:
+			cout << "Bitte gib nur eine der unten stehenden Zahlen an" << endl;
+			cout << "1 = Bisasam        2 = Bisaknosp        3 = Bisaflor"
+					<< endl;
+			cin >> input;
+			break;
+		}
+	}
 
+    cout << "Herzlichen Glückwunsch. Du hast "<< pokedex[auswahl-1].name <<" ausgewählt. Das ist eine gute Wahl"<<endl;
+    spieler.pokemon1 = pokedex[auswahl-1];
 
     return 0;
 }
