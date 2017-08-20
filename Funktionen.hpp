f91c41c17fd0d8910a92d1d53593e07f2d4c341c
JStupka <jacob_stupka@web.de>
2017-08-20 12:50:17 +0200

    Update and rename Funktionen.cpp to Funktionen.hpp

--- Funktionen.hpp
+++ Funktionen.hpp
@@ -4,32 +4,28 @@
 #include <tuple>
 #include <stdlib.h>
 #include <time.h>
-#include "Spieler.cpp"
+#include "Spieler.hpp"
 
 
-
-int checktype(string type1, string type2){//liste mit typschwächen, wenn typen final
-	return 1;//momentan alles sehr effektiv
-}
 
 int attackalgorithm(Pokemon pokpact, Pokemon pokeact){//entscheide, welche Attacke Gegner verwendet
-	if(pokeact.mana<pokeact.attack1.manacost){//teste, ob zureichend Mana für Angriff
+	if(pokeact.mana<pokeact.attack1.manacost){//teste, ob zureichend Mana fÃ¼r Angriff
 		if(pokeact.mana<pokeact.attack2.manacost){
-			return 0;//falls Mana unzureichend für beide, nutze Verzweifler
+			return 0;//falls Mana unzureichend fÃ¼r beide, nutze Verzweifler
 		}
 		else{
-			return 2;//zu wenig Mana für 1-->Angriff 2
+			return 2;//zu wenig Mana fÃ¼r 1-->Angriff 2
 		}
 	}
 	if(pokeact.mana<pokeact.attack2.manacost){
-		return 1;//zu wenig Mana für 2-->Angriff 1
+		return 1;//zu wenig Mana fÃ¼r 2-->Angriff 1
 	}
-	else{//genug Mana für beide
+	else{//genug Mana fÃ¼r beide
 		int r=rand()%100+1;
 		int r1;
-		int sus1=checktype(pokeact.attack1.type,pokpact.type);//prüfe auf Typanfälligkeiten
+		int sus1=checktype(pokeact.attack1.type,pokpact.type);//prÃ¼fe auf TypanfÃ¤lligkeiten
 		int sus2=checktype(pokeact.attack2.type,pokpact.type);
-		if(sus1>sus2){//falls anfälliger auf Angriff 1 als auf 2, nutze mit p=70% Angriff 1
+		if(sus1>sus2){//falls anfÃ¤lliger auf Angriff 1 als auf 2, nutze mit p=70% Angriff 1
 			r1=70;
 		}
 		if(sus2>sus1){//70% Angriff 2
@@ -49,20 +45,23 @@ int attackalgorithm(Pokemon pokpact, Pokemon pokeact){//entscheide, welche Attac
 
 
 int combatroutine(Spieler spieler, Spieler enemy){
+	cout << "angekommen" << endl;
 	bool flightparameter=false;
 	int pact;
 	int eact=1;
-	char choice;//wähle Diener
+	char choice;//wÃ¤hle Diener
+	cout << "Wähle Pokemon" << endl;
 	cin >> choice;
 	switch(choice){
 	case '1': pact=1; break;//player-active
 	case '2': pact=2; break;
 	case '3': pact=3; break;
 	}
+	cout << "gewählt" << endl;
 	do{//Matchroutine (3vs3) bis alle Diener eines Kontrahenten besiegt sind
 		Pokemon pokpact;
 		Pokemon pokeact;
-		switch(pact){//temporäre Diener im Zwischenspeicher
+		switch(pact){//temporÃ¤re Diener im Zwischenspeicher
 		case 1: pokpact=spieler.pokemon1; break;
 		case 2: pokpact=spieler.pokemon2; break;
 		case 3: pokpact=spieler.pokemon3; break;
@@ -72,33 +71,38 @@ int combatroutine(Spieler spieler, Spieler enemy){
 		case 2: pokeact=enemy.pokemon2; break;
 		case 3: pokeact=enemy.pokemon3; break;
 		}
-		do{//einzelne Kampfroutine (1vs1) bis ein Kämpfer besiegt ist
+		do{//einzelne Kampfroutine (1vs1) bis ein KÃ¤mpfer besiegt ist
 			int enaction=1;
 			char whichaction;
+			cout << "Wähle Aktion (f,h,m,1,2)" << endl;
 			cin >> whichaction;//frage Spieleraktion ab
-			if(whichaction='f'){
+			if(whichaction=='f'){
 				flightparameter=true;
 				break;
 			}
-			if(whichaction='h'){
+			if(whichaction=='h'){
 				spieler.usepotion(0, pokpact);
+				cout << "healthpotion" << endl;
 			}
-			if(whichaction='m'){
+			if(whichaction=='m'){
 				spieler.usepotion(1, pokpact);
+				cout << "manapotion" << endl;
 			}
-			if(pokeact.health<30 & enemy.inventory[0]>0){//Gegner nutzt Tränke, falls nötig, und verbraucht dabei seine Aktion
+			if(pokeact.health<30 & enemy.inventory[0]>0){//Gegner nutzt TrÃ¤nke, falls nÃ¶tig, und verbraucht dabei seine Aktion
 				enemy.usepotion(0,pokeact);
+				cout << "enemypotion" << endl;
 				enaction=0;
 			}
-			if(enaction=1 & pokeact.mana<pokeact.attack1.manacost & pokeact.mana<pokeact.attack2.manacost & enemy.inventory[1]>0){
+			if(enaction==1 & pokeact.mana<pokeact.attack1.manacost & pokeact.mana<pokeact.attack2.manacost & enemy.inventory[1]>0){
 				enemy.usepotion(1,pokeact);
 				enaction=0;
 			}
 			else{
+				cout << "schlagabtausch" << endl;
 				pair<double,string> ang;
 				double xptemp;
 				if(pokeact.init>pokpact.init){
-					if(enaction=1){
+					if(enaction==1){
 						int attackswitch=attackalgorithm(pokpact,pokeact);
 						switch(attackswitch){
 						case 0: break;
@@ -123,9 +127,9 @@ int combatroutine(Spieler spieler, Spieler enemy){
 					case '1': ang=pokpact.angriff(pokpact.attack1); break;
 					case '2': ang=pokpact.angriff(pokpact.attack2); break;
 					}
-					xptemp=pokeact.verteidigen(ang);;
+					xptemp=pokeact.verteidigen(ang);
 					pokpact.gainXP(xptemp);
-					if(enaction=1 & pokeact.health>0){
+					if(enaction==1 & pokeact.health>0){
 						int attackswitch=attackalgorithm(pokpact,pokeact);
 						switch(attackswitch){
 						case 0: break;
@@ -136,8 +140,8 @@ int combatroutine(Spieler spieler, Spieler enemy){
 					}
 				}
 			}
-		}while(pokpact.health>0 & pokeact.health>0);//bis ein Kämpfer besiegt
-		switch(pact){//temporäre Daten werden permanent
+		}while(pokpact.health>0 & pokeact.health>0);//bis ein KÃ¤mpfer besiegt
+		switch(pact){//temporÃ¤re Daten werden permanent
 		case 1: spieler.pokemon1=pokpact; break;
 		case 2: spieler.pokemon2=pokpact; break;
 		case 3: spieler.pokemon3=pokpact; break;
@@ -150,10 +154,11 @@ int combatroutine(Spieler spieler, Spieler enemy){
 		if(flightparameter==true){
 			break;
 		}
-		if(pokpact.health>0){//nächster Diener wird eingesetzt
+		if(pokpact.health>0){//nÃ¤chster Diener wird eingesetzt
 			eact+=1;
 		}
-		else{//neuer Diener wird ausgewählt
+		else{//neuer Diener wird ausgewÃ¤hlt
+			cout << "wähle neues pokemon" << endl;
 			cin >> choice;
 			switch(choice){
 			case '1': pact=1; break;
@@ -161,7 +166,7 @@ int combatroutine(Spieler spieler, Spieler enemy){
 			case '3': pact=3; break;
 			}
 		}
-	}while((enemy.pokemon1.health+enemy.pokemon2.health+enemy.pokemon3.health>0) & (spieler.pokemon1.health+spieler.pokemon2.health+spieler.pokemon3.health>0));//bis alle Kämpfer besiegt
+	}while((enemy.pokemon1.health+enemy.pokemon2.health+enemy.pokemon3.health>0) & (spieler.pokemon1.health+spieler.pokemon2.health+spieler.pokemon3.health>0));//bis alle KÃ¤mpfer besiegt
 	if(flightparameter==true){
 		return 0;//Flucht ist keine Niederlage!
 	}
