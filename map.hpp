5e5ffbfa9382aa6b90373538001f23d95f36f492
Yann-Paul <yannpaulm@gmail.com>
2017-09-19 03:35:35 +0200

    Update map.hpp

--- map.hpp
+++ map.hpp
@@ -1,6 +1,6 @@
 //Using SDL and standard IO
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 #include <string>
 #include <stdio.h>
